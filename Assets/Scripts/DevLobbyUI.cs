c2be6dd9af3d5f80e524d70df87acba34de5e98b
Alexander Pham <108191659+pham-t-alex@users.noreply.github.com>
2025-09-27 23:55:38 -0700

    dev lobby and separate work scenes

---
+++ Assets/Scripts/DevLobbyUI.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DevLobbyUI : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField sceneInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+
+    public void StartGame()
+    {
+        if (NetworkManager.Singleton.IsHost)
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene(sceneInput.text, UnityEngine.SceneManagement.LoadSceneMode.Single);
+        }
+    }
+}
