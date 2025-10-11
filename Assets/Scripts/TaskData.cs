1cf15b080389deb2c56b3d24b8945a8891c68adc
Alexander Pham <108191659+pham-t-alex@users.noreply.github.com>
2025-10-11 13:53:46 -0700

    starting pet tasks

---
+++ Assets/Scripts/TaskData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public abstract class TaskData : ScriptableObject
+{
+    [Header("Task Info")]
+    [field: SerializeField, Tooltip("The task name")]
+    public string TaskName { get; private set; }
+    [field: SerializeField, Tooltip("Task point value")]
+    public int PointValue { get; private set; }
+    [field: SerializeField, Tooltip("Task max completion value")]
+    public float MaxCompletionValue { get; private set; }
+    [field: SerializeField, Tooltip("Task starting value")]
+    public float StartCompletionValue { get; private set; }
+
+    public abstract PetTask CreateTask();
+}
